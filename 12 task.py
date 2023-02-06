2f30b8b3e3e62c7eae58f13b6340b6aec46eb356
Iramihed <imihed@mail.ru>
2023-02-06 21:40:27 +0300

    Revert "mathematical operations"
    This reverts commit c489c2d43930fd48ae9ad03d06c7bfa5faeb6d8d.

--- 12 task.py
+++
@@ -1,6 +0,0 @@
-# Вычислить факториал введённого числа.
-a=int(input("Ввелите число: "))
-factorial=1
-for i in range(1,a+1):
-    factorial=factorial*i
-print("Факториал числа", a, "равен", factorial)
